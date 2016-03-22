7a17c3a3bc7cb98d142bc9da3a0a8690bd117e0f
leorunwind <luorunwen@qq.com>
2016-03-22 11:34:35 +0800

    最大连续子数组问题，采用分治策略，时间复杂度为O(nlgn)

---
+++ maxSubarray.py
@@ -0,0 +1,45 @@
+def maxSubarray(arr,low,high):
+    #找出数组里的最大连续子数组,返回最大子数组的边界及和，时间复杂度为O(nlgn)
+    #注意形参里的high是最大下标
+    if(low == high):
+        return (low,high,arr[low])
+    
+    else:
+        mid = int((low+high)/2)
+        (left_low,left_high,left_sum) = maxSubarray(arr,low,mid)
+        (right_low,right_high,right_sum) = maxSubarray(arr,mid+1,high)
+        (cross_low,cross_high,cross_sum) = maxCrossSubarray(arr,low,mid,high)#最大子数组跨越中点的情况
+        
+        #比较三种情况的最大者
+        if left_sum>=right_sum and left_sum>=cross_sum:
+            return (left_low,left_high,left_sum)
+        elif right_sum>=left_sum and right_sum>=cross_sum:
+            return (right_low,right_high,right_sum)
+        else:
+            return (cross_low,cross_high,cross_sum)
+            
+
+def maxCrossSubarray(arr,low,mid,high):
+    #注意形参里的high是最大下标
+    left_sum = -float('inf')
+    sum = 0
+    for i in range(mid,low-1,-1):
+        sum += arr[i]
+        if sum>left_sum:
+            left_sum = sum
+            left_index = i
+            
+    right_sum = -float('inf')
+    sum = 0
+    for j in range(mid+1,high+1):
+        sum += arr[j]
+        if sum>right_sum:
+            right_sum = sum
+            right_index = j
+            
+    return (left_index,right_index,left_sum+right_sum)#返回最大连续子数组的下标及和
+    
+#测试用例    
+arr = [3,-1,2,4,-2]
+res_tuple = maxSubarray(arr,0,len(arr)-1)
+print(res_tuple)
